efe85c4f6122e45672f3f16f5d0a4a857f13ef00
Rubeun Tan <github@rubeun.com>
2022-02-16 14:55:24 -0800

    Use MUI theme to style default theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 import NavBar from './components/NavBar';
 import { getAuth } from './api/spotify';
 
 import Home from './routes/Home';
+
+const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        colorPrimary: {
+          backgroundColor: '#222'
+        }
+      }
+    }
+  }
+});
 
 function App() {
   const [authorised, setAuthorised] = useState(false);
@@ -18,7 +31,7 @@ function App() {
   if (!authorised) return null;
 
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Router>
         <NavBar />
         <Routes>
@@ -26,7 +39,7 @@ function App() {
           
         </Routes>
       </Router>
-    </div>
+    </ThemeProvider>    
   );
 }
 
