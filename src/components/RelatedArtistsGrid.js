fae7f05a6f79cb179f1a94c300b343934d838d46
Rubeun Tan <github@rubeun.com>
2022-03-08 14:57:01 -0800

    Add Link to each related artist item to their own artist info

--- src/components/RelatedArtistsGrid.js
+++ src/components/RelatedArtistsGrid.js
@@ -1,25 +1,51 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { 
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  styled,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+const ImageLink = styled(Link)(({ theme }) => ({
+  color: '#50df84',
+  textDecoration: 'none',
+  '&:hover': {
+    color: 'white',
+  }
+}))
+
+const ArtistImage = styled('img')(({ theme }) => ({
+  width: '160px',
+  height: '160px',
+  objectFit: 'contain',
+  '&:hover': {
+    opacity: 0.7,
+  }
+}))
+
+
 
 const RelatedArtistsGrid = ({ relatedArtists }) => {
   console.log(relatedArtists);
   return (
     <ImageList cols={4} gap={8}>
       {relatedArtists.map((artist, index) => (
-        <ImageListItem key={index}>
-          <img
-            src={artist.images[2].url}
-            alt={artist.name}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={artist.name}
-            subtitle={<span>followers: {artist.followers.total}</span>}
-            position="below"
-          />
-        </ImageListItem>
+        <ImageLink to={`/artist/${artist.id}`}>
+          <ImageListItem key={index}>
+            <ArtistImage
+              src={artist.images[2].url}
+              alt={artist.name}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={artist.name}
+              subtitle={<span>followers: {artist.followers.total}</span>}
+              position="below"
+            />
+          </ImageListItem>
+        </ImageLink>
       ))}
     </ImageList>
   );
